05e90c44cb846474631a1db03f438a63e36635d2
TenkyuChimata <tenkyuchimata@gmail.com>
2025-04-04 11:40:07 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,7 @@
       <v-container class="d-flex align-center py-0">
         <v-app-bar-title class="pl-0">
           <div class="d-flex align-center">
-            <v-avatar
-              class="me-3"
-              image="images/Vite.png"
-              rounded="0"
-            />
+            <v-avatar class="me-3" image="images/logo.png" rounded="0" />
 
             Wolfx Project
           </div>
@@ -16,25 +12,19 @@
 
         <v-spacer />
 
-        <v-btn-alt
-          :text="currentLanguage.nativeName"
-          @click="toggleLanguage"
-        />
+        <v-btn-alt :text="currentLanguage.nativeName" @click="toggleLanguage" />
 
       </v-container>
     </v-app-bar>
 
     <v-main>
       <section id="hero">
-        <v-sheet
-          class="d-flex align-center pb-16"
-          color="grey-darken-3"
-        >
+        <v-sheet class="d-flex align-center pb-16" color="grey-darken-3">
           <v-container class="text-center">
             <v-responsive class="mx-auto" width="500">
               <v-img
-                src="images/Vite.png"
-                height="400"
+                src=""
+                height="100"
               />
 
               <h3 class="text-h3">
@@ -75,21 +65,13 @@
                     {{ $t('WolfxOpenApiIntroduce4') }}
                   </p>
 
-                  <v-btn
-                    class="mt-6"
-                    href="https://wolfx.jp/apidoc"
-                    target="_blank"
-                    rel="noopener"
-                  >
+                  <v-btn class="mt-6" href="https://wolfx.jp/apidoc" target="_blank" rel="noopener">
                     {{ $t('OpenApiDoc') }}
                   </v-btn>
                 </v-responsive>
               </v-col>
 
-              <v-img
-                max-width="400"
-                src="images/API.png"
-              />
+              <v-img max-width="400" src="images/API.png" />
             </v-row>
           </v-container>
         </section>
@@ -119,21 +101,13 @@
                     {{ $t('WREMIntroduce3') }}
                   </p>
 
-                  <v-btn
-                    class="mt-6"
-                    href="https://bs.wolfx.jp/about.html"
-                    rel="noopener"
-                    target="_blank"
-                  >
+                  <v-btn class="mt-6" href="https://bs.wolfx.jp/about.html" rel="noopener" target="_blank">
                     {{ $t('WREMDoc') }}
                   </v-btn>
                 </v-responsive>
               </v-col>
 
-              <v-img
-                max-width="400"
-                src="images/WREM.png"
-              />
+              <v-img max-width="400" src="images/WREM.png" />
             </v-row>
           </v-container>
         </section>
@@ -159,38 +133,24 @@
                     {{ $t('ACGKRIntroduce2') }}
                   </p>
 
-                  <v-btn
-                    class="mt-6"
-                    href="https://acg.kr"
-                    rel="noopener"
-                    target="_blank"
-                  >
+                  <v-btn class="mt-6" href="https://acg.kr" rel="noopener" target="_blank">
                     {{ $t('ACGKRDoc') }}
                   </v-btn>
                 </v-responsive>
               </v-col>
 
-              <v-img
-                max-width="400"
-                src="images/ACGKR.png"
-              />
+              <v-img max-width="400" src="images/ACGKR.png" />
             </v-row>
           </v-container>
         </section>
       </v-sheet>
 
-      <v-sheet
-        class="py-16"
-        color="#181818"
-      >
+      <v-sheet class="py-16" color="#181818">
         <section id="grid">
           <v-container>
             <v-row justify="space-between">
               <v-col cols="auto">
-                <v-responsive
-                  class="overflow-visible"
-                  width="350"
-                >
+                <v-responsive class="overflow-visible" width="350">
                   <h2 class="text-h4">
                     Wolfx SeisJS
                   </h2>
@@ -203,21 +163,12 @@
                     {{ $t('seisjsIntroduce2') }}
                   </p>
 
-                  <v-btn-alt
-                    class="mt-6 "
-                    href="https://wolfx.jp/seisjs/"
-                    target="_blank"
-                    rel="noopener"
-                    :text="$t('seisjsDoc')"
-                  />
+                  <v-btn-alt class="mt-6 " href="https://wolfx.jp/seisjs/" target="_blank" rel="noopener"
+                    :text="$t('seisjsDoc')" />
                 </v-responsive>
               </v-col>
 
-              <v-img
-                max-width="400"
-                max-height="350"
-                src="images/seisJS.png"
-              />
+              <v-img max-width="400" max-height="350" src="images/seisJS.png" />
             </v-row>
           </v-container>
         </section>
@@ -229,12 +180,12 @@
         <div>
           Copyright &copy; 2017-{{ (new Date()).getFullYear() }} Wolfx Project
           <div>Links <a href="https://www.yorushi.com/" target="_blank" rel="noopener">
-            {{ $t('link1') }}
-          </a> <a href="https://bousai.cn/" target="_blank" rel="noopener">
-            {{ $t('link2') }}
-          </a> <a href="https://n0w0n.com/" target="_blank" rel="noopener">
-            {{ $t('link3') }}
-          </a></div>
+              {{ $t('link1') }}
+            </a> <a href="https://bousai.cn/" target="_blank" rel="noopener">
+              {{ $t('link2') }}
+            </a> <a href="https://n0w0n.com/" target="_blank" rel="noopener">
+              {{ $t('link3') }}
+            </a></div>
         </div>
 
         <div>
@@ -250,34 +201,34 @@
 </template>
 
 <script>
-  export default {
-    name: 'TranslationShowCase',
-    data () {
-      return {
-        languages: {
-          ja: { nativeName: '日本語 | Lang' },
-          'zh-CN': { nativeName: '简体中文 | Lang' },
-          en: { nativeName: 'English | Lang' },
-        },
-        currentLanguageCode: 'ja',
-      }
-    },
-    computed: {
-      currentLanguage () {
-        return this.languages[this.currentLanguageCode]
+export default {
+  name: 'TranslationShowCase',
+  data() {
+    return {
+      languages: {
+        ja: { nativeName: '日本語 | Lang' },
+        'zh-CN': { nativeName: '简体中文 | Lang' },
+        en: { nativeName: 'English | Lang' },
       },
+      currentLanguageCode: 'ja',
+    }
+  },
+  computed: {
+    currentLanguage() {
+      return this.languages[this.currentLanguageCode]
     },
-    methods: {
-      toggleLanguage () {
-        const codes = Object.keys(this.languages)
-        const index = codes.indexOf(this.currentLanguageCode)
-        this.currentLanguageCode = codes[(index + 1) % codes.length]
-        this.$i18next.changeLanguage(this.currentLanguageCode)
-      },
+  },
+  methods: {
+    toggleLanguage() {
+      const codes = Object.keys(this.languages)
+      const index = codes.indexOf(this.currentLanguageCode)
+      this.currentLanguageCode = codes[(index + 1) % codes.length]
+      this.$i18next.changeLanguage(this.currentLanguageCode)
     },
-  }
+  },
+}
 </script>
 
 <script setup>
-  //
+//
 </script>
